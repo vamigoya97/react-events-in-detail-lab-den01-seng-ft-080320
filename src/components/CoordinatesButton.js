2e7432f8ee8bcaeb068338d592d059624bc1cc02
vamigoya97 <amigo.victor.97@gmail.com>
2020-10-13 14:15:16 -0400

    lab completed

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+export default class CoordinatesButton extends Component {
+
+    eventHandler = (event) => {
+        const coordArray = [event.clientX, event.clientY]
+        this.props.onReceiveCoordinates(coordArray)
+    }
+
+    render() {
+        return( 
+            <button
+            onClick={this.eventHandler}
+            >Get Coordinates</button>
+        )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,17 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+
+    eventHandler = (event) => {
+        setTimeout(this.props.onDelayedClick, this.props.delay, event.target)
+    }
+
+    render() {
+        return (
+            <button
+            onClick={this.eventHandler}
+            >Get Delayed Event</button>
+        )
+    }
+}
